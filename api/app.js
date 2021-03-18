dfddeadbeaf82a5a61b15d765580a18012fd133c
Lucas Sachet <lucasachet@gmail.com>
2021-03-18 09:42:10 -0300

    Delete and save HTTP to MongoDB

--- api/app.js
+++ api/app.js
@@ -13,25 +13,42 @@ mongoose.connect('mongodb+srv://lucassachet:T62anmRtpPZx8r2@cluster0.ofbqy.mongo
 app.use(express.json({ limit: '50mb' }));
 
 app.post('/api/stores', (req,res) => {
-  let dbStores = req.body; 
-  console.log(dbStores);
-  var store = new Store({
-    storeName: "Test",
-    phoneNumber: "99231365",
-    location: {
-      type: 'Point',
-      coordinates: [
-        -118.363715,
-        34.057968
-      ]
+  let dbStores = [];
+  let stores = req.body;
+  stores.forEach((store) => {
+    dbStores.push({
+      storeName: store.name,
+      phoneNumber: store.phoneNumber,
+      adress: store.adress,
+      openStatusText: store.openStatusText,
+      adressLines: store.adressLines,
+      location: {
+        type: 'Point',
+        coordinates: [
+          store.coordinates.longitude,
+          store.coordinates.latitude
+        ]
+      }
+    })
+  }) 
+
+  Store.create(dbStores, (err, stores) =>{
+    if(err){
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(stores);
     }
   })
-  store.save()
-  res.send("you have posted")
 } )
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.delete('/api/stores', (req, res) => {
+  Store.deleteMany({}, (err) => {
+    res.status(200).send(err);
+  })
 })
 
 app.listen(port, () => {
